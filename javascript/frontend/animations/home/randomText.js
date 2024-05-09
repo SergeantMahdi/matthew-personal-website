2586faaae2c1e749e3be71d0442e9f5dbc4dac42
Mahdi Sartipzadeh <Mahdi.sartipzadeh@gmail.com>
2024-05-08 21:35:33 -0700

    fix the bug of loading text ouside the scope

--- javascript/frontend/animations/home/randomText.js
+++ javascript/frontend/animations/home/randomText.js
@@ -1,19 +1,21 @@
-const container = document.querySelector('.hero')
 const animationSection = document.querySelector('.characterAnimation');
-const charactersArray = ["</>", "#C14B09", "JS", "function", "||", "&&", "HTML5", "*", "Const", "Math.random()", "++", "STD::COUT", "DIV"];
+const charactersArray = ["</>", "#C14B09", "TRUE", "function", "||", "&&", "HTML5", "STD::THREAD", "Const", "Math.random()", "EVENT", "STD::COUT", "DIV"];
 
-let height = container.clientHeight;
-let width = container.clientWidth;
 
 let randomTextAnimation = () => {
+    //Get the size of animationSection to scope the characters
+    let height = animationSection.offsetHeight - 100;
+    let width = animationSection.offsetWidth - 200;
+    //Make a random number for x and y postion
     let xPosition = Math.floor(Math.random() * width) + 1;
     let yPosition = Math.floor(Math.random() * height) + 1;
+
+    //make random number for characters inside the array
     let rand = Math.floor(Math.random() * charactersArray.length) + 0;
     let duration = Math.random() * 3;
     let randChar = charactersArray[rand];
 
     let element = document.createElement('div');
-
     animationSection.appendChild(element);
     element.innerHTML = randChar;
     element.style.animationDirection = duration;
@@ -25,4 +27,4 @@ let randomTextAnimation = () => {
     }, 2900)
 }
 
-setInterval(randomTextAnimation, 200);
+setInterval(randomTextAnimation, 300);
