5d7daa5aa6c8c96d2c0120692779a34b301a8510
Mahdi Sartipzadeh <Mahdi.sartipzadeh@gmail.com>
2024-06-14 10:24:00 -0700

    Change: add admin route and project Post route

--- index.js
+++ index.js
@@ -4,6 +4,24 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const engine = require('ejs-mate');
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/Mahdi')
+    .then(() => console.log(color.green("Mongoose is connected")))
+    .catch(error => console.log(color.bgRed("MONGOOSE ERROR: ", error)));
+
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+    name: String,
+    description: String,
+    image: String,
+    link: String
+})
+
+const projectDB = mongoose.model("Project", projectSchema);
+
+
 
 const app = express();
 const port = 3000;
@@ -27,7 +45,15 @@ app.get('/contact', function(req, res) {
 app.get('/projects', function(req, res) {
     res.render('pages/project');
 });
-app.get('*', function(req, res) {
+
+app.post('/projects', function(req, res) {
+    
+});
+
+app.get('/admin', function(req, res) {
+    res.render('pages/admin');
+});
+app.all('*', function(req, res) {
     res.render('pages/404page');
 });
 
