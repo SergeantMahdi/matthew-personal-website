bcd32697bf4b6dc994df493294fe220e4fa95916
Mahdi Sartipzadeh <Mahdi.sartipzadeh@gmail.com>
2024-06-16 01:17:48 -0700

    New: API to fetch data with progress bars and a route to add new skill

--- index.js
+++ index.js
@@ -4,22 +4,20 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const engine = require('ejs-mate');
 
+/*Database*/
 const mongoose = require('mongoose');
+const projectDB = require("./models/projectSchema.js");
+
+/*APIs*/
+const { homePageFetch, projectPageFetch, skillCardFetch } = require("./APIs/fetchData.js");
+
+/*Functions*/
+const { createProject } = require("./controller/projectDatabase.js");
+const { createSkill } = require("./controller/skillDatabase.js");
 
 mongoose.connect('mongodb://127.0.0.1:27017/Mahdi')
     .then(() => console.log(color.green("Mongoose is connected")))
     .catch(error => console.log(color.bgRed("MONGOOSE ERROR: ", error)));
-
-const Schema = mongoose.Schema;
-
-const projectSchema = new Schema({
-    name: String,
-    description: String,
-    image: String,
-    link: String
-})
-
-const projectDB = mongoose.model("Project", projectSchema);
 
 
 
@@ -35,48 +33,32 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', function(req, res) {
-    res.render('pages/home');
+    res.render('pages/home' ,{title:"Mahdi Sartipzadeh"});
 });
 app.get('/about', function(req, res) {
-    res.render('pages/about');
+    res.render('pages/about', {title:"About Me"});
 });
+
+const skillDB = require("./models/skillSchema.js");
+
+app.post('/about', createSkill)
+
 app.get('/contact', function(req, res) {
-    res.render('pages/contact');
+    res.render('pages/contact', {title:"Contact Me"});
 });
 app.get('/projects', async function (req, res) {
-    res.render('pages/project');
+    res.render('pages/project', {title:"My Projects"});
 });
-app.get('/api-project/projects', async function (req, res) {
-    try {
-        const data = await projectDB.find({});
-        res.json(data);
-    } catch (err) {
-        res.status(500).send("Error fetching projects");
-    }
-});
-app.get('/api-project/', async function (req, res) {
-    try {
-        const data = await projectDB.find({}).sort({createdAt: -1}).limit(6);
-        res.json(data);
-    } catch (err) {
-        res.status(500).send("Error fetching projects");
-    }
-});
-
-app.post('/projects', async function (req, res) {
-    const newProject = new projectDB({
-    name: req.body.projectName,
-    description: req.body.projectDescription,
-    image: req.body.projectImage,
-    link: req.body.projectLink
-    })
-    await newProject.save();
-    res.render('pages/project');
-});
+app.post('/projects', createProject);
 
 app.get('/admin', function(req, res) {
-    res.render('pages/admin');
+    res.render('pages/admin', {title:"Admin"});
 });
+
+app.get('/api-project/projects', projectPageFetch);
+app.get('/api-project/',homePageFetch );
+app.get('/api-skill/about',skillCardFetch );
+
 app.all('*', function(req, res) {
     res.render('pages/404page');
 });
