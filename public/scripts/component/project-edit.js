cde40e3666777130ba231a398c17c3904225733a
Mahdi Sartipzadeh <Mahdi.sartipzadeh@gmail.com>
2024-06-17 06:49:26 -0700

    Change: remove unnecessary class from input

--- public/scripts/component/project-edit.js
+++ public/scripts/component/project-edit.js
@@ -1,17 +1,18 @@
-const projectSection = document.querySelector('.projectSelection');
+
 
 async function fetchProjects(api) {
     try {
         const response = await fetch(api);
         const data = await response.json();
-        makeCard(data);
+        makeProjectCard(data);
     } catch (error) {
         console.error('Error fetching projects:', error);
     }
 }
 
-function makeCard(data) {
+function makeProjectCard(data) {
     data.forEach(element => {
+        const projectSection = document.querySelector('.projectSelection');
         const projectContainer = document.createElement('div');
         projectContainer.classList.add("projectContainer");
         projectSection.appendChild(projectContainer);
@@ -19,18 +20,16 @@ function makeCard(data) {
         radioInput.classList.add("selection");
         radioInput.setAttribute('type', "radio");
         radioInput.setAttribute('name', "selection");
-        radioInput.setAttribute('id', "selection");
         projectContainer.appendChild(radioInput);
         const label = document.createElement('label');
         projectContainer.appendChild(label);
-        label.setAttribute('for', "selection");
         label.innerText = element.name;
 
         radioInput.addEventListener('change', () => {
-            document.querySelector('#editName').setAttribute('value', element.name);
-            document.querySelector('#editDescription').innerText = element.description;
-            document.querySelector('#editImage').setAttribute('value', element.image);
-            document.querySelector('#editLink').setAttribute('value', element.link);
+            document.querySelector('#PName').setAttribute('value', element.name);
+            document.querySelector('#PDescription').innerText = element.description;
+            document.querySelector('#PImage').setAttribute('value', element.image);
+            document.querySelector('#PLink').setAttribute('value', element.link);
             document.querySelector('#projectId').setAttribute('value', element._id);
         })
     });
