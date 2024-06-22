f97479299e063ba13bd84f808a1467cc7f2d6701
Mahdi Sartipzadeh <Mahdi.sartipzadeh@gmail.com>
2024-06-22 12:01:09 -0700

    New: user login validation

--- middleware/schemaValidate.js
+++ middleware/schemaValidate.js
@@ -1,6 +1,4 @@
-const { projectSchema, skillSchema } = require('../controller/joi.js')
-const skillsDB = require('../models/skillSchema.js');
-const projectsDB = require('../models/projectSchema.js');
+const { projectSchema, skillSchema, userSchema } = require('../controller/joi.js')
 const ExpressError = require('../utilities/expressError.js');
 
 
@@ -16,6 +14,17 @@ module.exports.validateSkill = (req, res, next) => {
 }
 module.exports.validateProject = (req, res, next) => {
     const { error } = projectSchema.validate(req.body);
+    if (error) {
+        const message = error.details.map(el => el.message).join(',');
+        throw new ExpressError(message, 400);
+    }
+    else {
+        next();
+    }
+}
+
+module.exports.validateUser = (req, res, next) => {
+    const { error } = userSchema.validate(req.body);
     if (error) {
         const message = error.details.map(el => el.message).join(',');
         throw new ExpressError(message, 400);
