0ed6b92a43c4f3d6c654f6559e611f228ba1c2f7
Mahdi Sartipzadeh <Mahdi.sartipzadeh@gmail.com>
2024-08-06 01:14:15 -0700

    Change: Limit login attempts

--- router/adminRoute.js
+++ router/adminRoute.js
@@ -4,6 +4,11 @@ const router = express.Router();
 const { isLoggedIn } = require('../middleware/schemaValidate.js');
 const { checkUser, createUser } = require("../controller/userDatabase.js");
 
+const requestLimition = limitReq({
+    windowsMs: 5 * 60 * 1000,
+    max: 100
+})
+route.use(requestLimition)
 router.route("/login21ma8")
     .get(function (req, res) {
         createUser();
@@ -17,6 +22,7 @@ router.route("/login21ma8")
     .post(checkUser);
 
 const { contactDB } = require("../models/contactSchema.js");
+const { route } = require('./projectsRoute.js');
 
 router.get('/admin21ma8', isLoggedIn, async function (req, res) {
     const contactData = await contactDB.find({}).sort({ created_at: -1 });
