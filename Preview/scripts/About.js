a22f3b5a7e97d7286e7320de9094014c61d6bd9e
Mahdi Sartipzadeh <Mahdi.sartipzadeh@gmail.com>
2025-06-05 19:21:59 +0330

    [REMOVE]: Remove all files to rebuild the project

--- Preview/scripts/About.js
+++
@@ -1,7 +0,0 @@
-const image = document.querySelector(".image");
-fetch("./images/AstroAbout.svg")
-  .then((response) => response.text())
-  .then((data) => {
-    image.innerHTML = data; // Inject the SVG code into the div
-  })
-  .catch((error) => console.error("Error loading SVG:", error));
--- Preview/scripts/disableImageSaving.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelectorAll('img').forEach(image => {
-    image.addEventListener("contextmenu", (e) => {
-        e.preventDefault();
-    })
-}
-)
