257aea25c1d2907b103bb2d571e8ebcb693328bf
Mahdi Sartipzadeh <Mahdi.sartipzadeh@gmail.com>
2025-06-30 12:47:49 +0330

    [CHANGE]: change the name of index.html to home.html and path from View/ to View/Home/

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,8 +1,21 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
+const root = resolve(__dirname, "Source");
+
 export default defineConfig({
-  root: "./Source/View",
+  root: "./Source",
+
   build: {
     outDir: "../Build",
+    rollupOptions: {
+      input: {
+        main: resolve(root, "View/index.html"),
+      },
+    },
+  },
+  server: {
+    open: "/View/index.html",
+    port: 3001,
   },
 });
