d93ac8c98163a2333036f4c103739427d9a2188b
Sergeant Matthew <mahdi.sartipzadeh@gmail.com>
2025-11-09 10:50:35 +0330

    fix[res.status]: change general status code with more specific one

--- server/middlewares/uploadFile.middleware.js
+++ server/middlewares/uploadFile.middleware.js
@@ -10,22 +10,24 @@ export default async function uploadSingleFile(req, res, next) {
         }
 
         if (error instanceof multer.MulterError) {
-            const errorMessage = "Failed to upload the file";
+            let errorMessage = "Failed to upload the file";
+            let statusCode = 500;
             if (error.code === "LIMIT_FILE_SIZE") {
                 errorMessage = "File size must be under 2MB";
+                statusCode = 413;
             }
-            return res.status(400).json({ message: errorMessage });
+            return res.status(statusCode).json({ message: errorMessage });
         }
 
         if (error) {
             if (error.message === "Invalid format") {
-                return res.status(400).json({ message: "File format is not valid" });
+                return res.status(415).json({ message: "File format is not valid" });
             }
             return res.status(500).json({ message: error.message });
         }
 
         if (!FileValidator.isFileSignatureValid(req.file)) {
-            return res.status(400).json({ message: "File format is not valid" });
+            return res.status(415).json({ message: "File format is not valid" });
         }
 
         next();
