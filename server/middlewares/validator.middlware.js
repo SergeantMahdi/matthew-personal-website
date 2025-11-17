a1fbdb6188d3a14d36d3386968d1e2388a262e11
Sergeant Matthew <mahdi.sartipzadeh@gmail.com>
2025-11-17 20:30:29 +0330

    faeture[]: add project input validator with joi

---
+++ server/middlewares/validator.middlware.js
@@ -0,0 +1,10 @@
+import projectSchema from "../schemas/project.schema";
+
+export function validateProjectInput(req, res, next) {
+    const { error } = projectSchema.validate(req.body);
+
+    if (error) {
+        res.status(400).json({ message: error.message });
+    }
+    next();
+};
