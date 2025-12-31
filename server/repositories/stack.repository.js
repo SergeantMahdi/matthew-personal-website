7f3bb54205987738d6335d58426bcad60bac1a12
Sergeant Matthew <mahdi.sartipzadeh@gmail.com>
2025-12-31 22:42:17 +0330

    refactor[findAll, findById]: add findAll and findById methods

--- server/repositories/stack.repository.js
+++ server/repositories/stack.repository.js
@@ -3,13 +3,23 @@ import StackModel from "../models/stack.model.js"
 class StackRepository {
 
     async create(name) {
-        const newStack = new StackModel({ name: name.toUpperCase() })
+        const newStack = new StackModel({ name })
         const result = await newStack.save();
         return result;
     }
 
+    async findAll() {
+        const foundStacks = await StackModel.find({}, { __V: 0 });
+        return foundStacks;
+    }
+
     async findOneByName(name) {
         const foundStack = await StackModel.findOne({ name: name.toUpperCase() });
+        return foundStack;
+    }
+
+    async findById(id) {
+        const foundStack = await StackModel.findById(id, { __V: 0 });
         return foundStack;
     }
 
@@ -22,8 +32,8 @@ class StackRepository {
         return result;
     }
 
-    async findByIdAndUpdate(id) {
-        const result = await StackModel.findByIdAndUpdate(id, { new: true });
+    async findByIdAndUpdate(id, name) {
+        const result = await StackModel.findByIdAndUpdate(id, { name }, { new: true });
         return result
     }
 
